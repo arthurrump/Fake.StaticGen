55e4021168d140f2d40a8e73dbea9e6c9dc87a84
Arthur Rump <hello@arthurrump.com>
2019-02-01 22:28:12 +0100

    Add Sass test application
    To verify that the problem is in the script environment, not in the
    code itself.

---
+++ src/TestApp/Program.fs
@@ -0,0 +1,10 @@
+﻿open Fake.StaticGen
+open Fake.StaticGen.Sass
+
+[<EntryPoint>]
+let main argv =
+    StaticSite.fromConfig ()
+    |> StaticSite.withSass "$mycolor: #000;\r\n body { color: $mycolor; }" "/style.css"
+    |> StaticSite.generateDry "output" (fun _ page -> page.Content)
+    |> printfn "%A"
+    0
