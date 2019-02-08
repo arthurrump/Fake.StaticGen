375fc087a19cc8b82b3dd1f85a2ba31f93aa5eb4
Arthur Rump <hello@arthurrump.com>
2019-02-08 21:28:04 +0100

    Try records based rss

--- src/Fake.StaticGen.Rss/Rss.fs
+++ src/Fake.StaticGen.Rss/Rss.fs
@@ -1,108 +1,118 @@
-﻿module Fake.StaticGen.Rss
+module Fake.StaticGen.Rss
 
-open System.Net
+open System
 
 type RssAttribute =
     | KeyValue of string * string
     | Boolean of string
 
-type RssElement = string * RssAttribute[]
+type Channel =
+    { Title : string
+      Link : string
+      Description : string
+      Items : Item list
+      Language : string option
+      Copyright : string option
+      ManagingEditor : string option
+      WebMaster : string option
+      PubDate : DateTime option
+      LastBuildDate : DateTime option
+      Category : Category option
+      Generator : string option
+      Docs : string option
+      Cloud : Cloud option
+      TTL : string option
+      Image : Image option
+      Rating : string option
+      TextInput : TextInput option
+      SkipHours : string option
+      SkipDays : string option }
 
-type RssNode =
-    | ParentNode of RssElement * RssNode list
-    | VoidElement of RssElement
-    | Text of string
+and Image =
+    { Url : string
+      Title : string
+      Link : string
+      Width : int option
+      Height : int option
+      Description : string option }
 
-let inline private encode v = WebUtility.HtmlEncode v
+and Cloud = 
+    { Domain : string
+      Port : int
+      Path : string
+      RegisterProcedure : string
+      Protocol : string }
 
-let attr key value = KeyValue (key, value)
-let flag key = Boolean key
+and TextInput = 
+    { Title : string
+      Description : string
+      Name : string
+      Link : string }
 
-let tag name attributes contents = ParentNode ((name, Array.ofList attributes), contents)
+and Item =
+    { Title : string option
+      Link : string option
+      Description : string option
+      Author : string option
+      Category : Category option
+      Comments : string option
+      Enclosure : Enclosure option
+      Guid : Guid option
+      PubDate : DateTime option
+      Source : string option }
 
-let voidTag name attributes = VoidElement (name, Array.ofList attributes)
+and Enclosure =
+    { Url : string
+      Length : string
+      Type : string }
 
-let rawText content = Text content
+and Category =
+    { Category : string
+      Domain : string option }
 
-let encodedText content = Text (encode content)
-let emptyText = rawText ""
-let comment content = rawText (sprintf "<!-- %s -->" content)
+and Guid =
+    { Guid : string 
+      IsPermaLink : bool option }
 
-let str = encodedText
-
-// Root channel tag
-let channel = tag "channel"
-
-// Required channel elements
-let title = tag "title"
-let link = tag "link"
-let description = tag "description"
-
-// Optional channel elements
-let language = tag "language"
-let copyright = tag "copyright"
-let managingEditor = tag "managingEditor"
-let webMaster = tag "webMaster"
-let pubDate = tag "pubDate"
-let lastBuildDate = tag "lastBuildDate"
-let category = tag "category"
-let generator = tag "generator"
-let docs = tag "docs"
-let cloud = tag "cloud"
-let ttl = tag "ttl"
-let image = tag "image"
-let rating = tag "rating"
-let textInput = tag "textInput"
-let skipHours = tag "skipHours"
-let skipDays = tag "skipDays"
-
-// Required elements on image
-let url = tag "url"
-// title
-// link
-
-// Optional elements on image
-let width = tag "width"
-let height = tag "height"
-// description
-
-// Attributes on cloud
-let _domain = attr "domain"
-let _port = attr "port"
-let _path = attr "path"
-let _registerProcedure = attr "registerProcedure"
-let _protocol = attr "protocol"
-
-// Required elements on textInput
-// title
-// description
-let name = tag "name"
-// link
-
-let item = tag "item"
-
-// Optional elements on item
-// title
-// link
-// description
-let author = tag "author"
-// category
-let comments = tag "comments"
-let enclosure = tag "enclosure"
-let guid = tag "guid"
-// pubDate
-let source = tag "source"
-
-// Required attribute on source
-let _url = attr "url"
-
-// Required attributes on enclosure
-// url
-let _length = attr "length"
-let _type = attr "type"
-
-// Optional attribute on category
-// domain
-
-// Optional attribute on guid
-let _isPermaLink = attr "isPermaLink"
+type Rss =
+      static member Channel
+                  (title : string,
+                   link : string,
+                   description : string,
+                   items : Item list,
+                   ?language : string,
+                   ?copyright : string,
+                   ?managingEditor : string,
+                   ?webMaster : string,
+                   ?pubDate : DateTime,
+                   ?lastBuildDate : DateTime,
+                   ?category : Category,
+                   ?generator : string,
+                   ?docs : string,
+                   ?cloud : Cloud,
+                   ?ttl : string,
+                   ?image : Image,
+                   ?rating : string,
+                   ?textInput : TextInput,
+                   ?skipHours : string,
+                   ?skipDays : string ) =
+            { Title = title
+              Link = link
+              Description = description
+              Items = items
+              Language = language
+              Copyright = copyright
+              ManagingEditor = managingEditor
+              WebMaster = webMaster
+              PubDate = pubDate
+              LastBuildDate = lastBuildDate
+              Category = category
+              Generator = generator
+              Docs = docs
+              Cloud = cloud
+              TTL = ttl
+              Image = image
+              Rating = rating
+              TextInput = textInput
+              SkipHours = skipHours
+              SkipDays = skipDays }
