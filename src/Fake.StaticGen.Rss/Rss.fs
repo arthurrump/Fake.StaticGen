5b99a960e4565759e6546d60e59ead848e71c5f9
Arthur Rump <hello@arthurrump.com>
2019-02-09 21:33:37 +0100

    Create RSS DSL with optional arguments
    Inspired by the Fabulous View DSL, using a derivative of the
    GiraffeViewEngine to actually create the XML

--- src/Fake.StaticGen.Rss/Rss.fs
+++ src/Fake.StaticGen.Rss/Rss.fs
@@ -1,118 +1,180 @@
-module Fake.StaticGen.Rss
+﻿namespace Fake.StaticGen.Rss
 
 open System
+open System.Text
 
-type RssAttribute =
-    | KeyValue of string * string
-    | Boolean of string
+open XmlEngine
 
-type Channel =
-    { Title : string
-      Link : string
-      Description : string
-      Items : Item list
-      Language : string option
-      Copyright : string option
-      ManagingEditor : string option
-      WebMaster : string option
-      PubDate : DateTime option
-      LastBuildDate : DateTime option
-      Category : Category option
-      Generator : string option
-      Docs : string option
-      Cloud : Cloud option
-      TTL : string option
-      Image : Image option
-      Rating : string option
-      TextInput : TextInput option
-      SkipHours : string option
-      SkipDays : string option }
-
-and Image =
-    { Url : string
-      Title : string
-      Link : string
-      Width : int option
-      Height : int option
-      Description : string option }
-
-and Cloud = 
-    { Domain : string
-      Port : int
-      Path : string
-      RegisterProcedure : string
-      Protocol : string }
-
-and TextInput = 
-    { Title : string
-      Description : string
-      Name : string
-      Link : string }
-
-and Item =
-    { Title : string option
-      Link : string option
-      Description : string option
-      Author : string option
-      Category : Category option
-      Comments : string option
-      Enclosure : Enclosure option
-      Guid : Guid option
-      PubDate : DateTime option
-      Source : string option }
-
-and Enclosure =
-    { Url : string
-      Length : string
-      Type : string }
-
-and Category =
-    { Category : string
-      Domain : string option }
-
-and Guid =
-    { Guid : string 
-      IsPermaLink : bool option }
+type Channel = private Channel of XmlNode
+type Image = private Image of XmlNode
+type Cloud = private Cloud of XmlNode
+type TextInput = private TextInput of XmlNode
+type Item = private Item of XmlNode
+type Enclosure = private Enclosure of XmlNode
+type Category = private Category of XmlNode
+type Guid = private Guid of XmlNode
+type Source = private Source of XmlNode
 
 type Rss =
-      static member Channel
-                  (title : string,
-                   link : string,
-                   description : string,
-                   items : Item list,
-                   ?language : string,
-                   ?copyright : string,
-                   ?managingEditor : string,
-                   ?webMaster : string,
-                   ?pubDate : DateTime,
-                   ?lastBuildDate : DateTime,
-                   ?category : Category,
-                   ?generator : string,
-                   ?docs : string,
-                   ?cloud : Cloud,
-                   ?ttl : string,
-                   ?image : Image,
-                   ?rating : string,
-                   ?textInput : TextInput,
-                   ?skipHours : string,
-                   ?skipDays : string ) =
-            { Title = title
-              Link = link
-              Description = description
-              Items = items
-              Language = language
-              Copyright = copyright
-              ManagingEditor = managingEditor
-              WebMaster = webMaster
-              PubDate = pubDate
-              LastBuildDate = lastBuildDate
-              Category = category
-              Generator = generator
-              Docs = docs
-              Cloud = cloud
-              TTL = ttl
-              Image = image
-              Rating = rating
-              TextInput = textInput
-              SkipHours = skipHours
-              SkipDays = skipDays }
+    static member Channel
+            (title : string,
+             link : string,
+             description : string,
+             items : Item list,
+             ?language : string,
+             ?copyright : string,
+             ?managingEditor : string,
+             ?webMaster : string,
+             ?pubDate : DateTime,
+             ?lastBuildDate : DateTime,
+             ?category : Category,
+             ?generator : string,
+             ?docs : string,
+             ?cloud : Cloud,
+             ?ttl : int,
+             ?image : Image,
+             ?rating : string,
+             ?textInput : TextInput,
+             ?skipHours : string,
+             ?skipDays : string) =
+        eTag "channel" [
+            yield eTag "title" [ str title ]
+            yield eTag "link" [ str link ]
+            yield eTag "description" [ str description ]
+            match language with Some x -> yield eTag "language" [ str x ] | _ -> ()
+            match copyright with Some x -> yield eTag "copyright" [ str x ] | _ -> ()
+            match managingEditor with Some x -> yield eTag "managingEditor" [ str x ] | _ -> ()
+            match webMaster with Some x -> yield eTag "webMaster" [ str x ] | _ -> ()
+            match pubDate with Some x -> yield eTag "pubDate" [ str (x.ToString("r")) ] | _ -> ()
+            match lastBuildDate with Some x -> yield eTag "lastBuildDate" [ str (x.ToString("r")) ] | _ -> ()
+            match category with Some (Category x) -> yield x | _ -> ()
+            match generator with Some x -> yield eTag "generator" [ str x ] | _ -> ()
+            match docs with Some x -> yield eTag "docs" [ str x ] | _ -> ()
+            match cloud with Some (Cloud x) -> yield x | _ -> ()
+            match ttl with Some x -> yield eTag "ttl" [ str (string x) ] | _ -> ()
+            match image with Some (Image x) -> yield x | _ -> ()
+            match rating with Some x -> yield eTag "rating" [ str x ] | _ -> ()
+            match textInput with Some (TextInput x) -> yield x | _ -> ()
+            match skipHours with Some x -> yield eTag "skipHours" [ str x ] | _ -> ()
+            match skipDays with Some x -> yield eTag "skipDays" [ str x ] | _ -> ()
+            yield eTag "items" (items |> List.map (fun (Item i) -> i))
+        ]
+        |> Channel
+
+    static member Image
+            (url : string,
+             title : string,
+             link : string,
+             ?width : int,
+             ?height : int,
+             ?description : string) =
+        eTag "image" [
+            yield eTag "url" [ str url ]
+            yield eTag "title" [ str title ]
+            yield eTag "link" [ str link ]
+            match width with Some x -> yield eTag "width" [ str (string x) ] | _ -> ()
+            match height with Some x -> yield eTag "height" [ str (string x) ] | _ -> ()
+            match description with Some x -> yield eTag "description" [ str x ] | _ -> ()
+        ]
+        |> Image
+
+    static member Cloud
+            (domain : string,
+             port : int,
+             path : string,
+             registerProcedure : string,
+             protocol : string) =
+        voidTag "cloud" [
+            attr "domain" domain
+            attr "port" (string port)
+            attr "path" path
+            attr "registerProcedure" registerProcedure
+            attr "protocol" protocol
+        ]
+        |> Cloud
+
+    static member TextInput
+            (title : string,
+             description : string,
+             name : string,
+             link : string) =
+        eTag "textInput" [
+            eTag "title" [ str title ]
+            eTag "description" [ str description ]
+            eTag "name" [ str name ]
+            eTag "link" [ str link ]
+        ]
+        |> TextInput
+
+    static member Item
+            (?title : string,
+             ?link : string,
+             ?description : string,
+             ?author : string,
+             ?category : Category,
+             ?comments : string,
+             ?enclosure : Enclosure,
+             ?guid : Guid,
+             ?pubDate : DateTime,
+             ?source : Source) =
+        eTag "item" [
+            match title with Some x -> yield eTag "title" [ str x ] | _ -> ()
+            match link with Some x -> yield eTag "link" [ str x ] | _ -> ()
+            match description with Some x -> yield eTag "description" [ str x ] | _ -> ()
+            match author with Some x -> yield eTag "author" [ str x ] | _ -> ()
+            match category with Some (Category x) -> yield x | _ -> ()
+            match comments with Some x -> yield eTag "comments" [ str x ] | _ -> ()
+            match enclosure with Some (Enclosure x) -> yield x | _ -> ()
+            match guid with Some (Guid x) -> yield x | _ -> ()
+            match pubDate with Some x -> yield eTag "pubDate" [ str (x.ToString("r")) ] | _ -> ()
+            match source with Some (Source x) -> yield x | _ -> ()
+        ]
+        |> Item
+
+    static member Enclosure
+            (url : string,
+             length : int,
+             ``type`` : string) =
+        voidTag "enclosure" [
+            attr "url" url
+            attr "length" (string length)
+            attr "type" ``type``
+        ]
+        |> Item
+
+    static member Category
+            (category : string,
+             ?domain : string) =
+        tag "category" 
+            [ match domain with Some x -> yield attr "domain" x | _ -> () ]
+            [ str category ]
+        |> Category
+    
+    static member Guid
+            (guid : string,
+             ?isPermaLink : bool) =
+        tag "guid"
+            [ match isPermaLink with Some x -> yield attr "isPermaLink" (string x) | _ -> () ]
+            [ str guid ]
+        |> Guid
+
+    static member Source
+            (source : string,
+             url : string) =
+        tag "source"
+            [ attr "url" url ]
+            [ str source ]
+        |> Source
+
+module Rss =
+    let renderFeed (Channel channel) =
+        let rssNode = tag "rss" [ attr "version" "2.0" ] [ channel ]
+        renderXmlDocument rssNode
+
+module StaticSite =
+    open Fake.StaticGen
+
+    let withRssFeed createChannel url site =
+        let channel = createChannel site
+        let file = { Url = url; Content = channel |> Rss.renderFeed }
+        site |> StaticSite.withFiles [ file ]
---
+++ src/Fake.StaticGen.Rss/XmlEngine.fs
@@ -0,0 +1,129 @@
+// ---------------------------
+// Attribution to original authors of this code
+// ---------------------------
+// This code has been originally ported from Suave to Giraffe and then
+// adapted in this project for general XML.
+//
+// Giraffe: Licensed under the Apache License, Version 2.0
+//          Copyright 2018 Dustin Moris Gorski and contributors
+// Suave:   Licensed under the Apache License, Version 2.0
+//          Copyright 2018 Ademar Gonzalez, Henrik Feldt and contributors
+// 
+// You can find the original implementations here:
+// https://github.com/SuaveIO/suave/blob/master/src/Experimental/Html.fs
+// https://github.com/giraffe-fsharp/Giraffe/blob/master/src/Giraffe/GiraffeViewEngine.fs
+
+module internal XmlEngine
+
+open System.Net
+open System.Text
+
+type XmlAttribute =
+    | KeyValue of string * string
+    | Boolean  of string
+
+type XmlElement   = string * XmlAttribute[]    // Name * XML attributes
+
+type XmlNode =
+    | ParentNode  of XmlElement * XmlNode list // An XML element which contains nested XML elements
+    | VoidElement of XmlElement                // An XML element which cannot contain nested XML (e.g. <hr /> or <br />)
+    | Text        of string                    // Text content
+
+// ---------------------------
+// Helper functions
+// ---------------------------
+
+let inline private encode v = WebUtility.HtmlEncode v
+
+// ---------------------------
+// Building blocks
+// ---------------------------
+
+let attr (key : string) (value : string) = KeyValue (key, encode value)
+let flag (key : string) = Boolean key
+
+let tag (tagName    : string)
+        (attributes : XmlAttribute list)
+        (contents   : XmlNode list) =
+    ParentNode ((tagName, Array.ofList attributes), contents)
+
+let voidTag (tagName    : string)
+            (attributes : XmlAttribute list) =
+    VoidElement (tagName, Array.ofList attributes)
+
+let inline eTag name = tag name []
+
+/// **Description**
+/// The `rawText` function will create an object of type `XmlNode` where the content will be rendered in its original form (without encoding).
+/// **Special Notice**
+/// Please be aware that the the usage of `rawText` is mainly designed for edge cases where someone would purposefully want to inject XML code into a rendered view. 
+/// If not used carefully this could potentially lead to serious security vulnerabilities and therefore should be used only when explicitly required.
+/// Most cases and particularly any user provided content should always be output via the `encodedText` function.
+let rawText     (content : string) = Text content
+
+/// **Description**
+/// The `encodedText` function will output a string where the content has been HTML encoded.
+let encodedText (content : string) = Text (encode content)
+let emptyText                      = rawText ""
+let comment     (content : string) = rawText (sprintf "<!-- %s -->" content)
+
+/// **Description**
+/// An alias for the `encodedText` function.
+let str = encodedText
+
+// ---------------------------
+// Build XML views
+// ---------------------------
+
+[<RequireQualifiedAccess>]
+module ViewBuilder =
+    let inline private (+=) (sb : StringBuilder) (text : string) = sb.Append(text)
+    let inline private (+!) (sb : StringBuilder) (text : string) = sb.Append(text) |> ignore
+
+    let [<Literal>] private selfClosingBracket = " />"
+
+    let rec private buildNode (sb : StringBuilder) (node : XmlNode) : unit =
+
+        let buildElement closingBracket (elemName, attributes : XmlAttribute array) =
+            match attributes with
+            | [||] -> do sb += "<" += elemName +! closingBracket
+            | _    ->
+                do sb += "<" +! elemName
+
+                attributes
+                |> Array.iter (fun attr ->
+                    match attr with
+                    | KeyValue (k, v) -> do sb += " " += k += "=\"" += v +! "\""
+                    | Boolean k       -> do sb += " " +! k)
+
+                do sb +! closingBracket
+
+        let inline buildParentNode (elemName, attributes : XmlAttribute array) (nodes : XmlNode list) =
+            do buildElement ">" (elemName, attributes)
+            for node in nodes do buildNode sb node
+            do sb += "</" += elemName +! ">"
+
+        match node with
+        | Text text             -> do sb +! text
+        | ParentNode (e, nodes) -> do buildParentNode e nodes
+        | VoidElement e         -> do buildElement selfClosingBracket e
+
+    let buildXmlNode = buildNode
+    let buildXmlNodes sb (nodes : XmlNode list) = for n in nodes do buildXmlNode sb n
+
+// ---------------------------
+// Render XML views
+// ---------------------------
+
+let renderXmlNode (node : XmlNode) : string =
+    let sb = new StringBuilder() in ViewBuilder.buildXmlNode sb node
+    sb.ToString()
+
+let renderXmlNodes (nodes : XmlNode list) : string =
+    let sb = new StringBuilder() in ViewBuilder.buildXmlNodes sb nodes
+    sb.ToString()
+
+let renderXmlDocument (node : XmlNode) : string =
+    let sb = StringBuilder().AppendLine("""<?xml version="1.0" encoding="UTF-8"?>""")
+    ViewBuilder.buildXmlNode sb node
+    sb.ToString()
