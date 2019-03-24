b483e6c88febfa702e2ae7be7872e990867fb295
Arthur Rump <hello@arthurrump.com>
2019-03-24 20:21:10 +0100

    Start adding Markdown support
    And tests for Markdown

---
+++ src/Fake.StaticGen.Markdown/Markdown.fs
@@ -0,0 +1,56 @@
+﻿namespace Fake.StaticGen.Markdown
+
+open Fake.Core
+open Fake.StaticGen
+open Markdig
+open System
+
+type MarkdownPage<'t> =
+    { Frontmatter : Page<'t>
+      RenderedMarkdown : string }
+
+module Markdown =
+    let splitFrontmatter markdown =
+        let concatn lines = String.concat Environment.NewLine lines
+        let lines = markdown |> String.splitStr Environment.NewLine
+        
+        let splitLines splitStr lines =
+            let first = lines |> Seq.takeWhile (fun l -> l <> splitStr)
+            let second = 
+                if Seq.length first = Seq.length lines 
+                then Seq.empty 
+                else lines |> Seq.skip (Seq.length first + 1)
+            first |> concatn, second |> concatn
+
+        match lines with
+        | "---"::tail ->
+            let f, c = tail |> splitLines "---"
+            Some f, c
+        | "+++"::tail -> 
+            let f, c = tail |> splitLines "+++"
+            Some f, c
+        | first::_ when first.StartsWith "{" -> 
+            let rec splitJson json (rest : string list) openBrackets =
+                match rest with
+                | [] -> json |> List.rev, []
+                | r::tail ->
+                    let chars = r.ToCharArray() |> Array.countBy id |> Map.ofArray
+                    let opens = chars |> Map.tryFind '{' |> Option.defaultValue 0
+                    let closes = chars |> Map.tryFind '}' |> Option.defaultValue 0
+                    let openBrackets = openBrackets + opens - closes
+                    if openBrackets < 1 then
+                        (r::json) |> List.rev, tail
+                    else
+                        splitJson (r::json) tail openBrackets
+            let f, c = splitJson [] lines 0
+            Some (f |> concatn), c |> concatn
+        | _ -> 
+            None, markdown
+
+module StaticSite =
+    let withPagesFromMarkdown markdownPages parseFrontmatter =
+        markdownPages
+        |> Seq.map (fun content ->
+            let frontmatter, markdown = Markdown.splitFrontmatter content
+            { Frontmatter = parseFrontmatter frontmatter
+              RenderedMarkdown = Markdown.ToHtml markdown })
---
+++ test/Fake.StaticGen.Test/Tests.fs
@@ -0,0 +1,107 @@
+﻿module Fake.StaticGen.Test
+
+open Expecto
+open System
+
+open Fake.StaticGen.Markdown
+
+let concatn lines = String.concat Environment.NewLine lines
+
+let tests =
+    testList "Markdown" [
+        test "Split YAML frontmatter" {
+            let markdown = 
+                [ "---"
+                  "key: value"
+                  "some text: '---'"
+                  "---"
+                  ""
+                  "# Hello world!" ] |> concatn
+
+            let frontmatter, content = Markdown.splitFrontmatter markdown
+            Expect.equal frontmatter ([ "key: value"; "some text: '---'" ] |> concatn |> Some) "Correct frontmatter"
+            Expect.equal content ([ ""; "# Hello world!" ] |> concatn) "Correct content"
+        } 
+
+        test "Split TOML frontmatter" {
+            let markdown = 
+                [ "+++"
+                  "key = \"+++\""
+                  "number = 5"
+                  "+++"
+                  ""
+                  "# Hello world!" ] |> concatn
+
+            let frontmatter, content = Markdown.splitFrontmatter markdown
+            Expect.equal frontmatter ([ "key = \"+++\""; "number = 5" ] |> concatn |> Some) "Correct frontmatter"
+            Expect.equal content ([ ""; "# Hello world!" ] |> concatn) "Correct content"
+        }
+
+        test "Split oneline JSON frontmatter" {
+            let markdown =
+                [ """{ "test": "value", "other": 24 }"""
+                  ""
+                  "# Hello world!" ] |> concatn
+
+            let frontmatter, content = Markdown.splitFrontmatter markdown
+            Expect.equal frontmatter (Some """{ "test": "value", "other": 24 }""") "Correct frontmatter"
+            Expect.equal content ([ ""; "# Hello world!" ] |> concatn) "Correct content"
+        }
+
+        test "Split multiline JSON frontmatter" {
+            let markdown =
+                [ "{"
+                  """  "test": "value","""
+                  """  "other": 24"""
+                  "}"
+                  ""
+                  "# Hello world!" ] |> concatn
+            
+            let frontmatter, content = Markdown.splitFrontmatter markdown
+            Expect.equal frontmatter ([ "{"; """  "test": "value","""; """  "other": 24"""; "}" ] |> concatn |> Some) 
+                "Correct frontmatter"
+            Expect.equal content ([ ""; "# Hello world!" ] |> concatn) "Correct content"
+        }
+
+        test "Split nested-object JSON frontmatter" {
+            let markdown =
+                [ "{"
+                  """  "test": {"""
+                  """    "other": 24"""
+                  "  }"
+                  "}"
+                  ""
+                  "# Hello world!" ] |> concatn
+            
+            let frontmatter, content = Markdown.splitFrontmatter markdown
+            Expect.equal frontmatter ([ "{"; """  "test": {"""; """    "other": 24"""; "  }"; "}" ] |> concatn |> Some)
+                "Correct frontmatter"
+            Expect.equal content ([ ""; "# Hello world!" ] |> concatn) "Correct content"
+        }
+
+        test "Split with no frontmatter" {
+            let markdown =
+                [ "# Hello world!"
+                  ""
+                  "Lorem ipsum dolor sit amet etc..." ] |> concatn
+            
+            let frontmatter, content = Markdown.splitFrontmatter markdown
+            Expect.equal frontmatter None "Correct frontmatter"
+            Expect.equal content markdown "Correct content"
+        }
+
+        test "Split unclosed TOML frontmatter" {
+            let markdown =
+                [ "+++"
+                  "value = \"hello\""
+                  "other = 23" ] |> concatn
+
+            let frontmatter, content = Markdown.splitFrontmatter markdown
+            Expect.equal frontmatter ([ "value = \"hello\""; "other = 23" ] |> concatn |> Some) "Correct frontmatter"
+            Expect.equal content "" "Correct content"
+        }
+    ]
+
+[<EntryPoint>]
+let main args = 
+    runTestsWithArgs defaultConfig args tests
