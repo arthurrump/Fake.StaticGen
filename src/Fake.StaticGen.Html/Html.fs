491d0ec9978b109a8cbea26898df13b13b95d1fe
Arthur Rump <hello@arthurrump.com>
2019-05-24 20:46:43 +0200

    Fix Html namespace/module conflict

--- src/Fake.StaticGen.Html/Html.fs
+++ src/Fake.StaticGen.Html/Html.fs
@@ -1,24 +1,12 @@
-﻿module Fake.StaticGen.Html
+﻿namespace Fake.StaticGen.Html
 
 open Fake.StaticGen
 open Fake.StaticGen.Html.ViewEngine
 
-type SiteBuilder<'config, 'components, 'page> with
-    [<CustomOperation("htmlTemplate")>]
-    member this.HtmlTemplate (state, template) =
-        let htmlRenderTemplate site page = template site page |> renderHtmlDocument
-        this.Template(state, htmlRenderTemplate)
-
-module StaticSite =
-    let site : SiteBuilderState<{|Title:string|}, obj, string> = 
-        staticsite {
-            config {| Title = "Arthur's Blog" |}
-
-            page "/" "Welcome home!"
-
-            htmlTemplate (fun helpers page -> 
-                html [] [ 
-                    head [] [ title [] [ str helpers.Config.Value.Title ] ]
-                    body [] [ str page.Content ] 
-                ])
-        }
+[<AutoOpen>]
+module Html =
+    type SiteBuilder<'config, 'components, 'page> with
+        [<CustomOperation("htmlTemplate")>]
+        member this.HtmlTemplate (state, template) =
+            let htmlRenderTemplate site page = template site page |> renderHtmlDocument
+            this.Template(state, htmlRenderTemplate)
