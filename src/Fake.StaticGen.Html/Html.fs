12bf5f2ef2d5254e79d2f40308c019e8eacf96cd
Arthur Rump <hello@arthurrump.com>
2019-05-24 16:19:42 +0200

    Start building ce-based version

--- src/Fake.StaticGen.Html/Html.fs
+++ src/Fake.StaticGen.Html/Html.fs
@@ -1,12 +1,24 @@
-﻿namespace Fake.StaticGen.Html
+﻿module Fake.StaticGen.Html
 
 open Fake.StaticGen
 open Fake.StaticGen.Html.ViewEngine
 
+type SiteBuilder<'config, 'components, 'page> with
+    [<CustomOperation("htmlTemplate")>]
+    member this.HtmlTemplate (state, template) =
+        let htmlRenderTemplate site page = template site page |> renderHtmlDocument
+        this.Template(state, htmlRenderTemplate)
+
 module StaticSite =
-    /// Generate the site and write it to the `outputPath` using an HTML 
-    /// template defined with a DSL based on the GiraffeViewEngine
-    let generateFromHtml outputPath template =
-        let renderFromHtml site page =
-            template site page |> renderHtmlDocument
-        StaticSite.generate outputPath renderFromHtml
+    let site : SiteBuilderState<{|Title:string|}, obj, string> = 
+        staticsite {
+            config {| Title = "Arthur's Blog" |}
+
+            page "/" "Welcome home!"
+
+            htmlTemplate (fun helpers page -> 
+                html [] [ 
+                    head [] [ title [] [ str helpers.Config.Value.Title ] ]
+                    body [] [ str page.Content ] 
+                ])
+        }
--- src/Fake.StaticGen/StaticSite.fs
+++ src/Fake.StaticGen/StaticSite.fs
@@ -37,143 +37,152 @@ module private UrlHelpers =
         let url = normalizeUrl url
         if System.IO.Path.HasExtension url then url else url + "/index.html"
 
-type Page<'content> =
-    { Url : string
-      Content : 'content }
-
-type StaticSite<'config, 'content> =
-    { BaseUrl : string
-      Config : 'config
-      Pages : Page<'content> seq
-      Files : Page<byte []> seq }
+type Helpers<'config, 'components> = 
+    { Config : 'config option
+      Components : Map<string, 'components>
+      BaseUrl : string }
 
     member this.AbsoluteUrl relativeUrl =
         this.BaseUrl + "/" + (normalizeUrl relativeUrl)
 
-type ISiteConfig =
-    abstract member BaseUrl : string
+type Page<'content> =
+    { Url : string
+      Content : 'content }
+
+type File = Page<byte []>
+
+type SourceParser<'content> = string -> string -> 'content
+
+type PageSource<'content> =
+    { Url : string 
+      Source : string 
+      Parser : SourceParser<'content> }
+
+type FileSource = 
+    { Url : string
+      Source : string }
+
+type Template<'config, 'comp, 'content> = Helpers<'config, 'comp> -> Page<'content> -> string
+
+[<RequireQualifiedAccess>]
+type Config<'config> =
+    | Object of 'config
+    | File of path : string * parse : (string -> 'config)
+
+[<NoComparison>]
+type SiteBuilderState<'config, 'components, 'page> = 
+    { Config : Config<'config> option
+      Template : Template<'config, 'components, 'page>
+      Pages : Page<'page> seq
+      PageSources : PageSource<'page> seq
+      Files : File seq
+      FileSources : FileSource seq }
+
+type SiteBuilder<'config, 'components, 'page> internal () =
+
+    member __.Yield (_) =
+        { Config = None
+          Template = fun _ page -> sprintf "No template set. Page contents:\n%A" page.Content
+          Pages = Seq.empty
+          PageSources = Seq.empty
+          Files = Seq.empty
+          FileSources = Seq.empty }
+
+    [<CustomOperation("config")>]
+    member __.Config (state, config : 'config) : SiteBuilderState<'config, 'components, 'page> =
+        { state with Config = Some (Config.Object config) }
+    
+    [<CustomOperation("configFile")>]
+    member __.ConfigFile (state, sourceFile : string, parse : (string -> 'config)) : SiteBuilderState<'config, 'components, 'page> =
+        { state with Config = Some (Config.File (sourceFile, parse)) }
+
+    [<CustomOperation("template")>]
+    member __.Template (state, template) : SiteBuilderState<'config, 'components, 'page> =
+        { state with Template = template }
+
+    [<CustomOperation("page")>]
+    member __.Page (state, url : string, content : 'page) : SiteBuilderState<'config, 'components, 'page> =
+        { state with Pages = state.Pages |> Seq.append [ { Url = url; Content = content } ] }
+
+    [<CustomOperation("pageSource")>]
+    member __.PageSource (state, pageSource) : SiteBuilderState<'config, 'components, 'page> =
+        { state with PageSources = state.PageSources |> Seq.append [ pageSource ] }
+
+    [<CustomOperation("pages")>]
+    member __.Pages (state, pages) : SiteBuilderState<'config, 'components, 'page> =
+        { state with Pages = state.Pages |> Seq.append pages }
+
+    [<CustomOperation("pageSources")>]
+    member __.PageSources (state, pageSources) : SiteBuilderState<'config, 'components, 'page> =
+        { state with PageSources = state.PageSources |> Seq.append pageSources }
+
+    [<CustomOperation("file")>]
+    member __.File (state, url, content) : SiteBuilderState<'config, 'components, 'file> =
+        { state with Files = state.Files |> Seq.append [ { Url = url; Content = content } ] }
+
+    [<CustomOperation("fileStr")>]
+    member __.File (state, url, content : string) : SiteBuilderState<'config, 'components, 'file> =
+        let bytes = Encoding.UTF8.GetBytes(content)
+        { state with Files = state.Files |> Seq.append [ { Url = url; Content = bytes } ] }
+
+    [<CustomOperation("fileSource")>]
+    member __.FileSource (state, fileSource : FileSource) : SiteBuilderState<'config, 'components, 'file> =
+        { state with FileSources = state.FileSources |> Seq.append [ fileSource ] }
+
+    [<CustomOperation("files")>]
+    member __.Files (state, files : #seq<File>) : SiteBuilderState<'config, 'components, 'file> =
+        { state with Files = state.Files |> Seq.append files }
+
+    [<CustomOperation("fileSources")>]
+    member __.FileSources (state, fileSources : #seq<FileSource>) : SiteBuilderState<'config, 'components, 'file> =
+        { state with FileSources = state.FileSources |> Seq.append fileSources }
+
+[<AutoOpen>]
+module SiteBuilder =
+    let staticsite<'config, 'components, 'page> = SiteBuilder<'config, 'components, 'page>()
 
 module StaticSite =
-    /// Create a new site with a base url and some configuration
-    let fromConfig baseUrl config =
-        { BaseUrl = normalizeUrl baseUrl
-          Config = config
-          Pages = Seq.empty
-          Files = Seq.empty }
+    let private prepareWrite url =
+        let path = pageUrlToFilePath url
+        Trace.tracefn "Writing %s" (Path.toRelativeFromCurrent path)
+        Directory.ensure (Path.getDirectory path)
+        path
 
-    /// Create a new site with a base url and some configuration parsed from a source file
-    let fromConfigFile baseUrl filePath parse =
-        Trace.tracefn "Reading %s" (Path.toRelativeFromCurrent filePath)
-        filePath |> File.readAsString |> parse |> fromConfig baseUrl
+    let private writePage url content =
+        let path = prepareWrite url
+        File.writeString false path content
 
-    /// Create a new site with some configuration that includes the BaseUrl
-    let fromIConfig (config : #ISiteConfig) =
-        fromConfig config.BaseUrl config
-    
-    /// Create a new site with some configuration that includes the BaseUrl 
-    /// parsed from a source file
-    let fromIConfigFile filePath parse =
-        Trace.tracefn "Reading %s" (Path.toRelativeFromCurrent filePath)
-        filePath |> File.readAsString |> parse |> fromIConfig
-
-    /// Add multiple pages
-    let withPages pages site =
-        let pages = pages |> Seq.map (fun p -> { p with Url = normalizeRelativeUrl p.Url })
-        { site with Pages = Seq.append pages site.Pages |> Seq.cache }
-
-    /// Add a new page
-    let withPage content url site =
-        let page = { Url = normalizeRelativeUrl url; Content = content }
-        site |> withPages [ page ]
-
-    /// Parse multiple source files and add them as pages
-    let withPagesFromSources sourceFiles parse site =
-        let pages = 
-            sourceFiles 
-            |> Seq.map (fun path -> 
-                Trace.tracefn "Reading %s" (Path.toRelativeFromCurrent path)
-                path |> File.readAsString |> parse path)
-        site |> withPages pages
-
-    /// Parse a source file and add it as a page
-    let withPageFromSource sourceFile parse site =
-        site |> withPagesFromSources [ sourceFile ] parse
-
-    /// Add multiple files
-    let withFiles files site =
-        let files = files |> Seq.map (fun f -> { f with Url = normalizeRelativeUrl f.Url })
-        { site with Files = Seq.append files site.Files |> Seq.cache }
-
-    /// Add a file
-    let withFile content url site =
-        let file = { Url = normalizeRelativeUrl url; Content = content }
-        site |> withFiles [ file ]
-
-    /// Add a file with text
-    let withFileFromString (content : string) =
-        withFile (Encoding.UTF8.GetBytes(content))
-
-    /// Copy multiple source files
-    let withFilesFromSources sourceFiles urlMapper site =
-        let files = 
-            sourceFiles 
-            |> Seq.map (fun path -> 
-                Trace.tracefn "Reading %s" (Path.toRelativeFromCurrent path)
-                let content = path |> File.readAsBytes
-                { Url = urlMapper path
-                  Content = content })
-        site |> withFiles files
-
-    /// Copy a source file
-    let withFileFromSource sourceFile url site =
-        site |> withFilesFromSources [ sourceFile ] (fun _ -> url)
-
-    /// Create an overview page based on the list of all pages
-    let withOverviewPage createOverview site =
-        let overview = Seq.delay (fun _ -> seq [ createOverview site ])
-        site |> withPages overview
-
-    /// Create multiple overview pages based on the list of all pages
-    let withOverviewPages createOverviewPages site =
-        let overview = Seq.delay (fun _ -> createOverviewPages site)
-        site |> withPages overview
-
-    /// Create a paginated overview with a specified number of items per page
-    let withPaginatedOverview itemsPerPage chooser createOverviewPages site =
-        let overview = 
-            Seq.delay (fun _ -> 
-                site.Pages 
-                |> Seq.choose chooser
-                |> Seq.chunkBySize itemsPerPage
-                |> createOverviewPages)
-        site |> withPages overview
-
-    /// Create an overview file based on the list of all pages, e.g. an RSS feed
-    let withOverviewFile (createOverview : StaticSite<'a, 'b> -> Page<string>) site =
-        let file = Seq.delay (fun _ -> 
-            let file = createOverview site
-            let content = Encoding.UTF8.GetBytes(file.Content)
-            seq [ { Url = file.Url; Content = content } ])
-        site |> withFiles file
-
-    // Dry run generate, returning a map of file paths and contents, instead of writing them out to disk
-    let generateDry outputPath (render : StaticSite<'a, 'b> -> Page<'b> -> string) site =
-        site.Pages
-        |> Seq.map (fun p ->
-            { Url = pageUrlToFilePath p.Url
-              Content = p |> render site |> Encoding.UTF8.GetBytes })
-        |> Seq.append site.Files
-        |> Seq.map (fun f ->
-            let url = normalizeUrl f.Url
-            let path = Path.combine outputPath url |> Path.normalizeFileName
-            path, f.Content)
+    let private writeFile url content =
+        let path = prepareWrite url
+        File.writeBytes path content
 
     /// Write the site files to the `outputPath`, using the render function to convert the pages into HTML
-    let generate outputPath render site =
+    let generate baseUrl outputPath site =
+        let config =
+            site.Config 
+            |> Option.map (fun conf -> 
+                match conf with
+                | Config.Object c -> c
+                | Config.File (path, parse) -> File.readAsString path |> parse)
+
+        let helpers = 
+            { Config = config
+              Components = Map.empty
+              BaseUrl = baseUrl }
+        
         Directory.delete outputPath
-        site
-        |> generateDry outputPath render
-        |> Seq.iter (fun (path, content) -> 
-            Trace.tracefn "Writing %s" (Path.toRelativeFromCurrent path)
-            Directory.ensure (Path.getDirectory path)
-            File.writeBytes path content)
+
+        site.Pages
+        |> Seq.iter (fun page -> site.Template helpers page |> writePage page.Url)
+
+        site.PageSources
+        |> Seq.iter (fun pagesrc ->
+            let content = File.readAsString pagesrc.Source |> pagesrc.Parser pagesrc.Source
+            let page = { Url = pagesrc.Url; Content = content }
+            site.Template helpers page |> writePage page.Url)
+
+        site.Files
+        |> Seq.iter (fun file -> writeFile file.Url file.Content)
+
+        site.FileSources
+        |> Seq.iter (fun filesrc -> File.readAsBytes filesrc.Source |> writeFile filesrc.Url)
